3bfeb9fa56b9b783ad38db2ad188bed070d89afc
Ray1422 <30434835+ray1422@users.noreply.github.com>
2020-03-26 05:13:22 +0800

    add Google Analytic

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,15 @@ $(document).ready(() => {
     $("#skill meter").each(function(){
     })
 })
+
+// Google Analytic
+$.ajax({
+    url: 'https://www.googletagmanager.com/gtag/js?id=UA-87019455-2',
+    dataType: 'script',
+    success: function(){
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-87019455-2');
+    }
+});

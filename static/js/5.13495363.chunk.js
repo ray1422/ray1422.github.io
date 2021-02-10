fef6c1aef53151ce5eece706ad6348d62923e79d
Ray1422 <ray20020809@gmail.com>
2021-02-11 01:35:19 +0800

    Updates

---
+++ static/js/5.13495363.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpray-another-world"]=this["webpackJsonpray-another-world"]||[]).push([[5],{52:function(e,t,c){"use strict";c.r(t);var s=c(0),r=(c(23),c(22)),n=(c(24),c(1));t.default=function(e){return Object(s.useEffect)((function(){e.setExpandHeader(!1)})),Object(n.jsx)("div",{className:"ani_item",children:Object(n.jsxs)(r.b,{children:[Object(n.jsx)("h2",{className:"text_texture crystal",children:"\u4f5c\u54c1\u53e2"}),"Collection",Object(n.jsx)("br",{}),"Hello",Object(n.jsx)("br",{})]})})}}}]);
+//# sourceMappingURL=5.13495363.chunk.js.map

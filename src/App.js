4760458b6d3766228525a0146a50a105156ccbd0
Ray Chung <ray20020809@gmail.com>
2021-02-16 02:37:45 +0800

    add theme detect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './css/container.css';
 
 import { OuterWrapper } from './components/container.js'
+if (window.matchMedia('(prefers-color-scheme: light)').matches === true) {
+    window.localStorage.theme = 'light'
+}
 export default (props) => {
     return (
         <OuterWrapper>
